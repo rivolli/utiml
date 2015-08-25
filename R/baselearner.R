d93de3b3f387872f38a0dc4f722ca6d11770557d
Adriano Rivolli <rivolli@gmail.com>
2015-08-25 14:34:08 -0300

    Add base methods CART, KNN and RandomForest and fix rownames from test examples

--- R/baselearner.R
+++ R/baselearner.R
@@ -45,7 +45,7 @@ mltrain.J48 <- function (dataset, ...) {
   if (requireNamespace("RWeka", quietly = TRUE)) {
     classname <- colnames(dataset$data)[ncol(dataset$data)]
     formula <- as.formula(paste("`", classname, "` ~ .", sep=""))
-    model <- RWeka::J48(formula, dataset$data)
+    model <- RWeka::J48(formula, dataset$data, ...)
   } else
     stop('There are no installed package "RWeka" to use J48 classifier as base method')
 
@@ -54,7 +54,7 @@ mltrain.J48 <- function (dataset, ...) {
 
 mlpredict.J48 <- function (model, newdata, ...) {
   if (requireNamespace("RWeka", quietly = TRUE)) {
-    result <- predict(model, newdata, "probability")
+    result <- predict(model, newdata, "probability", ...)
   } else
     stop('There are no installed package "RWeka" to use J48 classifier as base method')
 
@@ -82,6 +82,48 @@ mlpredict.C5.0 <- function (model, newdata, ...) {
   getResultPrediction(probability = result[,"1"])
 }
 
+#CART
+mltrain.CART <- function (dataset, ...) {
+  if (requireNamespace("rpart", quietly = TRUE)) {
+    classname <- colnames(dataset$data)[ncol(dataset$data)]
+    formula <- as.formula(paste("`", classname, "` ~ .", sep=""))
+    model <- rpart::rpart(formula, dataset$data, ...)
+  } else
+    stop('There are no installed package "rpart" to use Cart classifier as base method')
+
+  model
+}
+
+mlpredict.CART <- function (model, newdata, ...) {
+  if (requireNamespace("rpart", quietly = TRUE)) {
+    result <- predict(model, newdata, type = "prob", ...)
+  } else
+    stop('There are no installed package "rpart" to use Cart classifier as base method')
+
+  getResultPrediction(probability = result[,"1"])
+}
+
+#Random Forest
+mltrain.RF <- function (dataset, ...) {
+  if (requireNamespace("randomForest", quietly = TRUE)) {
+    traindata <- dataset$data[, -ncol(dataset$data)]
+    labeldata <- dataset$data[, dataset$labelname]
+    model <- randomForest::randomForest(traindata, labeldata, ...)
+  } else
+    stop('There are no installed package "randomForest" to use randomFores classifier as base method')
+
+  model
+}
+
+mlpredict.RF <- function (model, newdata, ...) {
+  if (requireNamespace("randomForest", quietly = TRUE)) {
+    result <- predict(model, newdata, type="prob", ...)
+  } else
+    stop('There are no installed package "randomForest" to use randomFores classifier as base method')
+
+  getResultPrediction(probability = result[,"1"])
+}
+
 #Naive Bayes
 mltrain.NB <- function (dataset, ...) {
   if (requireNamespace("e1071", quietly = TRUE)) {
@@ -99,6 +141,30 @@ mlpredict.NB <- function (model, newdata, ...) {
     result <- predict(model, newdata, type = "raw", ...)
   } else
     stop('There are no installed package "e1071" to use naiveBayes classifier as base method')
-
+  rownames(result) <- rownames(newdata)
   getResultPrediction(probability = result[,"1"])
+}
+
+#Knn - consider others packages (FNN and KKNN)
+mltrain.KNN <- function (dataset, ...) {
+  if (!requireNamespace("class", quietly = TRUE))
+    stop('There are no installed package "class" to use kNN classifier as base method')
+  dataset$extrakNN <- list(...)
+  dataset
+}
+
+mlpredict.KNN <- function (model, newdata, ...) {
+  if (requireNamespace("class", quietly = TRUE)) {
+    traindata <- model$data[, -ncol(model$data)]
+    labeldata <- model$data[, model$labelname]
+    args <- list(...)
+    result <- if (is.null(model$extrakNN[["k"]]) || !is.null(args[["k"]]))
+        class::knn(traindata, newdata, labeldata, prob=T, ...)
+      else
+        class::knn(traindata, newdata, labeldata, k=model$extrakNN[["k"]], prob=T, ...)
+    result <- ifelse(result == 0, 1-attr(result, "prob"), attr(result, "prob"))
+  } else
+    stop('There are no installed package "class" to use kNN classifier as base method')
+  names(result) <- rownames(newdata)
+  getResultPrediction(probability = result)
 }
