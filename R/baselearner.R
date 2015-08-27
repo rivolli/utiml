ee5529bcac1a08ada692e1dc69792c92bf3c4787
Adriano Rivolli <rivolli@gmail.com>
2015-08-27 17:26:11 -0300

    Fix mltrain/mlpredict and some initial documentation

--- R/baselearner.R
+++ R/baselearner.R
@@ -1,23 +1,110 @@
-getResultPrediction <- function (bipartition = NULL, probability = NULL) {
-  emptyBipartition <- is.null(bipartition)
-  emptyProbability <- is.null(probability)
-  if (emptyBipartition && emptyProbability)
-    stop("Bipartion and probability results are NULL")
-  else if (emptyProbability)
-    probability <- bipartition
-  else {
-    bipartition <- probability
-    active <- bipartition >= 0.5
-    bipartition[active] <- 1
-    bipartition[!active] <- 0
-  }
+#' @title Create a predictive result object
+#'
+#' @description The transformation methods require a specific data format from base
+#'  classifiers prediction. If you implement a new base method then use this method
+#'  to return the final result of your \code{mlpredict} method.
+#'
+#' @param probability A vector with probabilities predictions or with bipartitions
+#'  prediction for a binary prediction.
+#' @param threshold A numeric value between 0 and 1 to create the bipartitions.
+#'
+#' @return An object of type "\code{mlresult}" used by problem transformation
+#'  methods that use binary classifiers. It has only two attributes:
+#'  \code{bipartition} and \code{probability}, that respectively have the
+#'  bipartition and probabilities results.
+#' @export
+#'
+#' @examples
+#' # This method is used to implement a mlpredict based method
+#' # In this example we create a random predict method
+#' mlpredicti.random <- function (model, newdata, ...) {
+#'    probs <- sample(seq(0,1,.025), nrow(newdata), replace = TRUE)
+#'    as.resultPrediction(probs)
+#' }
+#'
+#' # Define a different threshold for a specific subproblem use
+#' ...
+#' result <- as.resultPrediction(probs, 0.6)
+#' ...
+as.resultPrediction <- function (probability, threshold = 0.5) {
+  bipartition <- probability
+  active <- bipartition >= threshold
+  bipartition[active] <- 1
+  bipartition[!active] <- 0
+
   res <- list(bipartition = bipartition, probability = probability)
   class(res) <- "mlresult"
   res
 }
 
+#' @title Train function to extend base classifiers
+#'
+#' @description
+#'  To extend a base classifier, two steps are necessary:
+#'  \enumerate{
+#'    \item Create a train method
+#'    \item Create a prediction method
+#'  }
+#'  This section is about how to create a train method. To create a new predict
+#'  model see \code{\link{mlpredict}} documentation.
+#'
+#' @section How to create a new train base method:
+#' Fist is necessary to define a name of your classifier because this name
+#' determine the method name, that must start with mltrain.base followed by your
+#' name, e.g. a "foo" classify must be \code{mltrain.basefoo}.
+#'
+#' After defined the name, you need to implement your base method. The dataset
+#' is available on \code{dataset$base}
+#'
+#'
+#'
+#' @param dataset An object of \code{mltransformation} class, that has at least
+#'  three attributes: \strong{data}, \strong{labelname} and \strong{methodname}.
+#'  The \code{data} is the dataframe with the predictive attributes and the
+#'  class column. The \code{labelname} is the name of the class column. Finally,
+#'  the \code{methodname} is the name of the implemented method.
+#' @param ... Others arguments passed to the base method.
+#'
+#' @return A model object. The class of this model can be of any type, however,
+#'  this object will be passed to the respective mlpredict method.
+#'
+#' @export
+#'
+#' @examples
+#' # Create a empty model of type foo
+#' mltrain.basefoo <- function (dataset, ...) {
+#'    mymodel <- list()
+#'    class(mymodel) <- "foomodel"
+#'    mymodel
+#' }
+#'
+#' #Use this base method with Binary Relevance
+#' brmodel <- br(emotions, "foo")
+#'
+#' # Create a SVM method using the e1071 package
+#' library(e1071)
+#' mltrain.baseSVM <- function (dataset, ...) {
+#'    traindata <- dataset$data[, -ncol(dataset$data)]
+#'    labeldata <- dataset$data[, dataset$labelname]
+#'    model <- svm(traindata, labeldata, probability = TRUE, ...)
+#'    model
+#' }
+mltrain <- function (dataset, ...) UseMethod("mltrain")
+
+mltrain.default <- function (dataset, ...) {
+  funcname <- paste("mltrain.base", dataset$methodname, sep='')
+  stop(paste("The function '", funcname, "(dataset, ...)' is not implemented", sep=''))
+}
+
+mlpredict <- function (model, newdata, ...) UseMethod("mlpredict")
+
+mlpredict.default <- function (model, newdata, ...) {
+  funcname <- paste("mlpredict.", class(model), sep='')
+  stop(paste("The function '", funcname, "(dataset, newdata, ...)' is not implemented", sep=''))
+}
+
 #Suport Vector Machines
-mltrain.SVM <- function (dataset, ...) {
+mltrain.baseSVM <- function (dataset, ...) {
   if (requireNamespace("e1071", quietly = TRUE)) {
     traindata <- dataset$data[, -ncol(dataset$data)]
     labeldata <- dataset$data[, dataset$labelname]
@@ -28,41 +115,41 @@ mltrain.SVM <- function (dataset, ...) {
   model
 }
 
-mlpredict.SVM <- function (model, newdata, ...) {
+mlpredict.svm <- function (model, newdata, ...) {
   if (requireNamespace("e1071", quietly = TRUE)) {
     result <- predict(model, newdata, probability = TRUE, ...)
   } else
     stop('There are no installed package "e1071" to use SVM classifier as base method')
 
-  pscore <- attr(result, "probabilities")[,"1"]
-  attr(result, "probabilities") <- NULL
-
-  getResultPrediction(result, pscore)
+  as.resultPrediction(attr(result, "probabilities")[,"1"])
 }
 
 #Decision Tree - J48
-mltrain.J48 <- function (dataset, ...) {
-  if (requireNamespace("RWeka", quietly = TRUE)) {
-    classname <- colnames(dataset$data)[ncol(dataset$data)]
-    formula <- as.formula(paste("`", classname, "` ~ .", sep=""))
+mltrain.baseJ48 <- function (dataset, ...) {
+  classname <- colnames(dataset$data)[ncol(dataset$data)]
+  formula <- as.formula(paste("`", classname, "` ~ .", sep=""))
+  if (requireNamespace("RWeka", quietly = TRUE))
     model <- RWeka::J48(formula, dataset$data, ...)
-  } else
-    stop('There are no installed package "RWeka" to use J48 classifier as base method')
+  else
+    stop('There are no installed package "RWeka" to use C4.5/J48 classifier as base method')
 
   model
 }
 
 mlpredict.J48 <- function (model, newdata, ...) {
-  if (requireNamespace("RWeka", quietly = TRUE)) {
+  if (requireNamespace("RWeka", quietly = TRUE))
     result <- predict(model, newdata, "probability", ...)
-  } else
-    stop('There are no installed package "RWeka" to use J48 classifier as base method')
+  else
+    stop('There are no installed package "RWeka" to use C4.5/J48 classifier as base method')
 
-  getResultPrediction(probability = result[,"1"])
+  as.resultPrediction(result[,"1"])
 }
 
+#Decision Tree - C4.5
+mltrain.baseC4.5 <- mltrain.baseJ48
+
 #Decision Tress - C5.0
-mltrain.C5.0 <- function (dataset, ...) {
+mltrain.baseC5.0 <- function (dataset, ...) {
   if (requireNamespace("C50", quietly = TRUE)) {
     traindata <- dataset$data[, -ncol(dataset$data)]
     labeldata <- dataset$data[, dataset$labelname]
@@ -79,11 +166,11 @@ mlpredict.C5.0 <- function (model, newdata, ...) {
   } else
     stop('There are no installed package "C50" to use C5.0 classifier as base method')
 
-  getResultPrediction(probability = result[,"1"])
+  as.resultPrediction(result[,"1"])
 }
 
 #CART
-mltrain.CART <- function (dataset, ...) {
+mltrain.baseCART <- function (dataset, ...) {
   if (requireNamespace("rpart", quietly = TRUE)) {
     classname <- colnames(dataset$data)[ncol(dataset$data)]
     formula <- as.formula(paste("`", classname, "` ~ .", sep=""))
@@ -94,17 +181,17 @@ mltrain.CART <- function (dataset, ...) {
   model
 }
 
-mlpredict.CART <- function (model, newdata, ...) {
+mlpredict.rpart <- function (model, newdata, ...) {
   if (requireNamespace("rpart", quietly = TRUE)) {
     result <- predict(model, newdata, type = "prob", ...)
   } else
     stop('There are no installed package "rpart" to use Cart classifier as base method')
 
-  getResultPrediction(probability = result[,"1"])
+  as.resultPrediction(result[,"1"])
 }
 
 #Random Forest
-mltrain.RF <- function (dataset, ...) {
+mltrain.baseRF <- function (dataset, ...) {
   if (requireNamespace("randomForest", quietly = TRUE)) {
     traindata <- dataset$data[, -ncol(dataset$data)]
     labeldata <- dataset$data[, dataset$labelname]
@@ -115,17 +202,17 @@ mltrain.RF <- function (dataset, ...) {
   model
 }
 
-mlpredict.RF <- function (model, newdata, ...) {
+mlpredict.randomForest <- function (model, newdata, ...) {
   if (requireNamespace("randomForest", quietly = TRUE)) {
     result <- predict(model, newdata, type="prob", ...)
   } else
     stop('There are no installed package "randomForest" to use randomFores classifier as base method')
 
-  getResultPrediction(probability = result[,"1"])
+  as.resultPrediction(result[,"1"])
 }
 
 #Naive Bayes
-mltrain.NB <- function (dataset, ...) {
+mltrain.baseNB <- function (dataset, ...) {
   if (requireNamespace("e1071", quietly = TRUE)) {
     traindata <- dataset$data[, -ncol(dataset$data)]
     labeldata <- dataset$data[, dataset$labelname]
@@ -136,24 +223,24 @@ mltrain.NB <- function (dataset, ...) {
   model
 }
 
-mlpredict.NB <- function (model, newdata, ...) {
+mlpredict.naiveBayes <- function (model, newdata, ...) {
   if (requireNamespace("e1071", quietly = TRUE)) {
     result <- predict(model, newdata, type = "raw", ...)
   } else
     stop('There are no installed package "e1071" to use naiveBayes classifier as base method')
   rownames(result) <- rownames(newdata)
-  getResultPrediction(probability = result[,"1"])
+  as.resultPrediction(result[,"1"])
 }
 
 #Knn - consider others packages (FNN and KKNN)
-mltrain.KNN <- function (dataset, ...) {
+mltrain.baseKNN <- function (dataset, ...) {
   if (!requireNamespace("class", quietly = TRUE))
     stop('There are no installed package "class" to use kNN classifier as base method')
   dataset$extrakNN <- list(...)
   dataset
 }
 
-mlpredict.KNN <- function (model, newdata, ...) {
+mlpredict.baseKNN <- function (model, newdata, ...) {
   if (requireNamespace("class", quietly = TRUE)) {
     traindata <- model$data[, -ncol(model$data)]
     labeldata <- model$data[, model$labelname]
@@ -166,5 +253,5 @@ mlpredict.KNN <- function (model, newdata, ...) {
   } else
     stop('There are no installed package "class" to use kNN classifier as base method')
   names(result) <- rownames(newdata)
-  getResultPrediction(probability = result)
+  as.resultPrediction(result)
 }
--- R/br.R
+++ R/br.R
@@ -1,3 +1,77 @@
+#' @title Binary Relevance for multi-label Classification
+#' @description Create a Binary Relevance model for multilabel classification.
+#'
+#'   Binary Relevance is a simple and effective transformation method to predict
+#'   multi-label data. This is based on the one-versus-all approach to build a
+#'   specific model for each label.
+#'
+#' @param mdata Object of class \code{\link[mldr]{mldr}}, a multi-label train
+#'   dataset (provided by \pkg{mldr} package).
+#' @param base.method A string or a named vector with the base method(s)
+#'   name(s). If a single value is passed the same base method will be used for
+#'   train all subproblems. If a named vector is passed then each subproblem can
+#'   be trained by a specific base method. When a named vector is used the size
+#'   and the name of its elements must have exactly the number and name of the
+#'   labels.
+#'
+#'   Default valid options are: \code{'SVM'}, \code{'C4.5'}, \code{'C5.0'},
+#'   \code{'RF'}, \code{'NB'} and \code{'KNN'}. To use other base method see
+#'   \href{https://github.com/rivolli/utiml}{online documentation}. (default:
+#'    \code{'SVM'}).
+#' @param ... Others arguments passed to the base method for all subproblems
+#'   (recommended only when the same base method is used for all labels).
+#' @param specific.params A named list to pass parameters for a specific model
+#'   (the name of the list define wich model will use the arguments) (default:
+#'   \code{list()}).
+#' @param save.datasets Logical indicating whether the binary datasets must be
+#'   saved in the model or not (default: FALSE).
+#' @param CORES The number of cores to parallelize the training. Values higher
+#'   than 1 require the \pkg{parallel} package (default: 1).
+#'
+#' @return An object of class \code{BRmodel} containing the set of fitted
+#'   models, including: \describe{ \item{labels}{A vector with the label names}
+#'   \item{models}{A list of models named by the label names. The model type is
+#'   defined by the base method used to train each subproblem} \item{datasets}{A
+#'   list of \code{mldBR} named by the label names. Only when the
+#'   \code{save.datasets = TRUE}.} }
+#'
+#' @section Warning:
+#'    RWeka package does not permit use \code{'C4.5'} in parallel mode, use
+#'    \code{'C5.0'} or \code{'CART'} instead of it
+#'
+#' @references
+#'  Boutell, M. R., Luo, J., Shen, X., & Brown, C. M. (2004). Learning
+#'    multi-label scene classification. Pattern Recognition, 37(9), 1757–1771.
+#'
+#' @export
+#'
+#' @examples
+#' # Train and predict emotion multilabel dataset using Binary Relevance
+#' library(utiml)
+#' testdata <- emotions$dataset[sample(1:100, 10), emotions$attributesIndexes]
+#'
+#' # Use SVM as base method
+#' model <- br(emotions)
+#' pred <- predict(model, testdata)
+#'
+#' # Change the default base method
+#' model <- br(emotions, "C4.5")
+#' pred <- predict(model, testdata)
+#'
+#' # Set a parameters for all subproblems
+#' model <- br(emotions, "KNN", k=5)
+#' pred <- predict(model, testdata)
+#'
+#' # Use differents base classifers for different labels and running in parallel
+#' methods <- c("SVM", "RF", "CART", "C5.0", "KNN", "RF")
+#' names(methods) <- rownames(emotions$labels)
+#' model <- br(emotions, methods, CORES=6)
+#' pred <- predict(model, testdata, CORES=6)
+#'
+#' # Change SVM kernel for label 'happy-pleased'
+#' extra <- list('happy-pleased' = list("kernel" = "linear"))
+#' model <- br(emotions, specific.params=extra)
+#' pred <- predict(model, testdata)
 br <- function (mdata,
                 base.method = "SVM",
                 ...,
@@ -20,9 +94,11 @@ br <- function (mdata,
   brmodel$labels = rownames(mdata$labels)
 
   #Relating Base methods with labels
-  if (length(base.method) != mdata$measures$num.labels)
+  if (length(base.method) != mdata$measures$num.labels) {
     base.method <- rep(base.method, mdata$measures$num.labels)
-  names(base.method) <- brmodel$labels
+    names(base.method) <- brmodel$labels
+  } else if (!all(names(base.method) %in% brmodel$labels))
+    stop("The names(base.method) must contain the name of all labels")
 
   #Transformation
   datasets <- lapply(mldr_transform(mdata), function (dataset) {
@@ -34,7 +110,7 @@ br <- function (mdata,
 
     #Create data
     dataset <- list(data = dataset, labelname = label, methodname = base.method[label])
-    class(dataset) <- "mldBR"
+    class(dataset) <- c("mldBR", paste("base", base.method[label], sep=''), "mltransformation")
 
     #Set specific parameters
     dataset$specific.params <- if (!is.null(specific.params[[label]])) specific.params[[label]]
@@ -55,11 +131,7 @@ br <- function (mdata,
     }
 
     #Call dynamic multilabel model with merged parameters
-    funcname <- paste("mltrain", dataset$methodname, sep=".")
-    if (!existsFunction(funcname))
-      stop(paste("The function '", funcname, "(dataset, ...)' is not implemented", sep=''))
-
-    model <- do.call(funcname, params)
+    model <- do.call(mltrain, params)
     attr(model, "BRlabel") <- dataset$labelname
     attr(model, "BRmethod") <- dataset$methodname
 
@@ -78,6 +150,48 @@ br <- function (mdata,
   brmodel
 }
 
+#' @title Predict Method for Binary Relevance
+#' @description This function predicts values based upon a model trained by \code{br}.
+#'
+#' @param object Object of class "\code{BRmodel}", created by \code{br}.
+#' @param newdata An object containing the new input data. This must be a matrix or
+#'          data.frame object containing the same size of training data.
+#' @param ... Others arguments passed to the base method prediction for all
+#'   subproblems (recommended only when the same base method is used for all labels).
+#' @param probability Logical indicating whether class probabilities should be returned.
+#'   (default: \code{TRUE})
+#' @param specific.params A named list to pass parameters for a specific model (the name
+#'   of the list define wich model will use the arguments) (default: \code{list()}).
+#' @param CORES The number of cores to parallelize the prediction. Values higher
+#'   than 1 require the \pkg{parallel} package (default: 1).
+#'
+#' @return A matrix containing the probabilistic values or just predictions (only when
+#'   \code{probability = FALSE}). The rows indicate the predicted object and the
+#'   columns indicate the labels.
+#'
+#' @section Warning:
+#'    RWeka package does not permit use \code{'C4.5'} in parallel mode, use
+#'    \code{'C5.0'} or \code{'CART'} instead of it
+#'
+#' @seealso \code{\link[=br]{Binary Relevance (BR)}}
+#'
+#' @export
+#'
+#' @examples
+#' library(utiml)
+#'
+#' # Emotion multi-label dataset using Binary Relevance
+#' testdata <- emotions$dataset[sample(1:100, 10), emotions$attributesIndexes]
+#'
+#' # Predict SVM scores
+#' model <- br(emotions)
+#' pred <- predict(model, testdata)
+#'
+#' # Predict SVM bipartitions running in 6 cores
+#' pred <- predict(model, testdata, probability = FALSE, CORES = 6)
+#'
+#' # Passing a specif parameter for SVM predict method
+#' pred <- predict(model, testdata, na.action = na.fail)
 predict.BRmodel <- function (object,
                              newdata,
                              ...,
@@ -94,19 +208,13 @@ predict.BRmodel <- function (object,
 
   predict_model <- function (model, ...) {
     label <- attr(model, "BRlabel")
-    method <- attr(model, "BRmethod")
 
     params <- c(list(model = model, newdata = newdata), ...)
     for (pname in names(specific.params[[label]])) {
       params[[pname]] <- specific.params[[label]][[pname]]
     }
 
-    #Call dynamic multilabel model with merged parameters
-    funcname <- paste("mlpredict", method, sep=".")
-    if (!existsFunction(funcname))
-      stop(paste("The function '", funcname, "(dataset, ...)' is not implemented", sep=''))
-
-    do.call(funcname, params)
+    do.call(mlpredict, params)
   }
 
   #Create models
@@ -122,10 +230,6 @@ predict.BRmodel <- function (object,
   rownames(result) <- names(predictions[[1]]$bipartition)
 
   result
-}
-
-getTestData <- function () {
-  emotions$dataset[sample(1:emotions$measures$num.instances, 10), emotions$attributesIndexes]
 }
 
 print.BRmodel <- function (x, ...) {
