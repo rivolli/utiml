74cdbe87cc3eef91853891eb8e0acf275d62d1cd
Adriano Rivolli <rivolli@gmail.com>
2015-12-04 18:07:33 -0200

    MBR refactory

--- R/method_mbr.R
+++ R/method_mbr.R
@@ -1,204 +1,198 @@
-#' @title Meta-BR or 2BR for multi-label Classification
+#' Meta-BR or 2BR for multi-label Classification
+#'
+#' Create a Meta-BR (MBR) classifier to predic multi-label data. To this, two
+#' round of Binary Relevance is executed, such that, the first step generates
+#' new attributes to enrich the second prediction.
+#'
+#' This implementation use complete training set for both training and
+#' prediction steps of 2BR. However, the \code{phi} parameter may be used to
+#' remove labels with low correlations on the second step.
+#'
 #' @family Transformation methods
 #' @family Stacking methods
-#' @description Create a Meta-BR (MBR) classifier to predic multi-label
-#'  data. To this, two round of Binary Relevance is executed, such that,
-#'  the first iteraction generates new attributes to enrich the second
-#'  prediction.
-#'
-#'  This implementation use complete training set for both training and
-#'  prediction steps of 2BR. However, the \code{phi} parameter may be used
-#'  to remove low label correlations on the second step.
-#'
-#' @param mdata Object of class \code{\link[mldr]{mldr}}, a multi-label train
-#'   dataset (provided by \pkg{mldr} package).
-#' @param base.method A string with the name of base method. The same base method
-#'   will be used for train all subproblems in the two rounds. This is slightly
-#'   different from the original paper that uses two distinct values from this
-#'   parameter.
-#'
-#'   Default valid options are: \code{'SVM'}, \code{'C4.5'}, \code{'C5.0'},
-#'   \code{'RF'}, \code{'NB'} and \code{'KNN'}. To use other base method see
-#'   \code{\link{mltrain}} and \code{\link{mlpredict}} instructions. (default:
-#'    \code{'SVM'})
-#' @param folds the number of folds used in internal prediction. If this value is
-#'    1 all dataset will be used to predict the first prediction. (default: 1)
+#' @param mdata A mldr dataset used to train the binary models.
+#' @param base.method A string with the name of the base method. (Default:
+#'  \code{options("utiml.base.method", "SVM")})
+#' @param folds The number of folds used in internal prediction. If this value
+#'  is 1 all dataset will be used in the first prediction. (Default: 1)
 #' @param phi A value between 0 and 1 to determine the correlation coefficient,
-#'    The value 0 include all labels in the second phase and the 1 only the
-#'    predicted label. A good value for this argument is 0.3 as suggest in
-#'    original paper. (default: 0)
+#'  The value 0 include all labels in the second phase and the 1 only the
+#'  predicted label. (Default: 0)
 #' @param ... Others arguments passed to the base method for all subproblems.
 #' @param predict.params A list of default arguments passed to the predictor
-#'  method. (default: \code{list()})
+#'  method. (Default: \code{list()})
 #' @param CORES The number of cores to parallelize the training. Values higher
-#'   than 1 require the \pkg{parallel} package. (default: 1)
-#'
+#'  than 1 require the \pkg{parallel} package. (Default:
+#'  \code{options("utiml.cores", 1)})
 #' @return An object of class \code{MBRmodel} containing the set of fitted
-#'   models, including: \describe{
+#'   models, including:
+#'   \describe{
 #'      \item{labels}{A vector with the label names.}
 #'      \item{phi}{The value of \code{phi} parameter.}
 #'      \item{correlation}{The matrix of label correlations used in combination
-#'        with \code{phi} parameter to define the labels used in the second step.
-#'      }
+#'        with \code{phi} parameter to define the labels used in the second
+#'        step. }
 #'      \item{basemodel}{The BRModel used in the first iteration.}
 #'      \item{models}{A list of models named by the label names used in the
-#'        second iteration.
-#'      }
-#'  }
-#'
+#'        second iteration. }
+#'   }
 #' @references
 #'  Tsoumakas, G., Dimou, A., Spyromitros, E., Mezaris, V., Kompatsiaris, I., &
-#'    Vlahavas, I. (2009). Correlation-based pruning of stacked binary relevance models
-#'    for multi-label learning. In Proceedings of the Workshop on Learning from
-#'    Multi-Label Data (MLD’09) (pp. 22–30).
+#'    Vlahavas, I. (2009). Correlation-based pruning of stacked binary relevance
+#'    models for multi-label learning. In Proceedings of the Workshop on
+#'    Learning from Multi-Label Data (MLD’09) (pp. 22–30).
 #'  Godbole, S., & Sarawagi, S. (2004). Discriminative Methods for Multi-labeled
 #'    Classification. In Data Mining and Knowledge Discovery (pp. 1–26).
-#' @seealso \code{\link{labels_correlation_coefficient}}
+#' @seealso \code{\link{calculate_labels_correlation}}
 #' @export
 #'
 #' @examples
-#' # Train and predict emotion multilabel dataset using Meta-BR
-#' dataset <- mldr_random_holdout(emotions, c(train=0.9, test=0.1))
-#'
+#' \dontrun{
 #' # Use SVM as base method
-#' model <- mbr(dataset$train)
-#' pred <- predict(model, dataset$test)
+#' model <- mbr(toyml)
+#' pred <- predict(model, toyml)
 #'
 #' # Use 10 folds and different phi correlation with C4.5 classifier
-#' model <- mbr(dataset$train, 'C4.5', 10, 0.3)
-#' pred <- predict(model, dataset$test)
+#' model <- mbr(toyml, 'C4.5', 10, 0.2)
 #'
 #' # Set a specific parameter
-#' model <- mbr(dataset$train, 'KNN', k=5)
-#' pred <- predict(model, dataset$test)
-mbr <- function(mdata, base.method = "SVM", folds = 1, phi = 0, ..., predict.params = list(), CORES = 1) {
-    # Validations
-    if (class(mdata) != "mldr")
-        stop("First argument must be an mldr object")
+#' model <- mbr(toyml, 'KNN', k=5)
+#' }
+mbr <- function(mdata, base.method = getOption("utiml.base.method", "SVM"),
+                folds = 1, phi = 0, ..., predict.params = list(),
+                CORES = getOption("utiml.cores", 1)) {
+  # Validations
+  if (class(mdata) != "mldr") {
+    stop("First argument must be an mldr object")
+  }
 
-    if (folds < 1)
-        stop("The number of folds must be positive")
+  if (folds < 1) {
+    stop("The number of folds must be positive")
+  }
 
-    if (phi < 0 || phi > 1)
-        stop("The phi threshold must be between 0 and 1, inclusive")
+  if (phi < 0 || phi > 1) {
+    stop("The phi threshold must be between 0 and 1, inclusive")
+  }
 
-    if (CORES < 1)
-        stop("Cores must be a positive value")
+  if (CORES < 1) {
+    stop("Cores must be a positive value")
+  }
 
-    # MBR Model class
-    mbrmodel <- list()
-    mbrmodel$labels <- rownames(mdata$labels)
-    mbrmodel$phi <- phi
+  # MBR Model class
+  mbrmodel <- list(labels = rownames(mdata$labels), phi = phi,
+                   call = match.call())
 
-    # 1 Iteration - Base Level
-    mbrmodel$basemodel <- br(mdata, base.method, ..., CORES = CORES)
+  # 1 Iteration - Base Level -------------------------------------------------
+  mbrmodel$basemodel <- br(mdata, base.method, ..., CORES = CORES)
+  if (folds == 1) {
+    params <- list(object = mbrmodel$basemodel,
+                   newdata = mdata$dataset[mdata$attributesIndexes],
+                   probability = FALSE, CORES = CORES)
+    base.preds <- do.call(predict, c(params, predict.params))
+  }
+  else {
+    kf <- create_kfold_partition(mdata, folds, "iterative")
+    base.preds <- do.call(rbind, lapply(seq(folds), function(f) {
+      dataset <- partition_fold(kf, f)
+      classifier <- br(dataset$train, base.method, ..., CORES = CORES)
+      params <- list(object = classifier, newdata = dataset$test,
+                     probability = FALSE, CORES = CORES)
+      do.call(predict, c(params, predict.params))
+    }))
+  }
 
-    if (folds == 1) {
-        params <- list(object = mbrmodel$basemodel, newdata = mdata$dataset[mdata$attributesIndexes], probability = FALSE, CORES = CORES)
-        base.preds <- do.call(predict, c(params, predict.params))
-    } else {
-        kf <- create_kfold_partition(mdata, folds, "iterative")
-        base.preds <- do.call(rbind, lapply(seq(folds), function(f) {
-            dataset <- partition_fold(kf, f)
-            classifier <- br(dataset$train, base.method, ..., CORES = CORES)
-            predict(classifier, dataset$test, prob = FALSE, CORES = CORES)
-        }))
-    }
+  # 2 Iteration - Meta level -------------------------------------------------
+  corr <- mbrmodel$correlation <- calculate_labels_correlation(mdata)
+  labels <- utiml_renames(mbrmodel$labels)
+  mbrmodel$models <- utiml_lapply(labels, function (label) {
+    new.col <- colnames(corr)[corr[label, ] > phi]
+    new.data <- base.preds[, new.col, drop = FALSE]
+    colnames(new.data) <- paste("extra", new.col, sep = ".")
+    brdata  <- create_br_data(mdata, label, new.data)
+    dataset <- prepare_br_data(brdata, "mldMBR", base.method,
+                               new.features = new.col)
+    create_br_model(dataset, ...)
+  }, CORES)
 
-    # 2 Iteration - Meta level
-    corr <- mbrmodel$correlation <- labels_correlation_coefficient(mdata)
-    br.datasets <- lapply(mldr_transform(mdata), prepare_br_data, classname = "mldBR", base.method = base.method)
-    names(br.datasets) <- mbrmodel$labels
-    datasets <- utiml_lapply(br.datasets, function(dataset) {
-        extracolumns <- base.preds[, colnames(corr)[corr[dataset$labelname, ] > phi], drop = FALSE]
-        colnames(extracolumns) <- paste("extra", colnames(extracolumns), sep = ".")
-        base <- cbind(dataset$data[-dataset$labelindex], extracolumns, dataset$data[dataset$labelindex])
-        prepare_br_data(base, "mldMBR", base.method, new.features = colnames(extracolumns))
-    }, CORES)
-    mbrmodel$models <- utiml_lapply(datasets, create_br_model, CORES, ...)
-
-    mbrmodel$call <- match.call()
-    class(mbrmodel) <- "MBRmodel"
-
-    mbrmodel
+  class(mbrmodel) <- "MBRmodel"
+  mbrmodel
 }
 
-#' @title Predict Method for Meta-BR/2BR
-#' @description This function predicts values based upon a model trained by \code{mbr}.
+#' Predict Method for Meta-BR/2BR
 #'
-#' @param object Object of class '\code{MBRmodel}', created by \code{\link{mbr}} method.
-#' @param newdata An object containing the new input data. This must be a matrix or
-#'          data.frame object containing the same size of training data or a mldr object.
+#' This function predicts values based upon a model trained by \code{mbr}.
+#'
+#' @param object Object of class '\code{MBRmodel}'.
+#' @param newdata An object containing the new input data. This must be a
+#'  matrix, data.frame or a mldr object.
+#' @param probability Logical indicating whether class probabilities should be
+#'  returned. (Default: \code{getOption("utiml.use.probs", TRUE)})
 #' @param ... Others arguments passed to the base method prediction for all
 #'   subproblems.
-#' @param probability Logical indicating whether class probabilities should be returned.
-#'   (default: \code{TRUE})
-#' @param CORES The number of cores to parallelize the prediction. Values higher
-#'   than 1 require the \pkg{parallel} package (default: 1).
-#'
-#' @return A matrix containing the probabilistic values or just predictions (only when
-#'   \code{probability = FALSE}). The rows indicate the predicted object and the
-#'   columns indicate the labels.
-#'
+#' @param CORES The number of cores to parallelize the training. Values higher
+#'  than 1 require the \pkg{parallel} package. (Default:
+#'  \code{options("utiml.cores", 1)})
+#' @return An object of type mlresult, based on the parameter probability.
 #' @seealso \code{\link[=mbr]{Meta-BR (MBR or 2BR)}}
 #' @export
 #'
 #' @examples
-#' # Emotion multi-label dataset using Meta-BR or 2BR
-#' dataset <- mldr_random_holdout(emotions, c(train=0.9, test=0.1))
-#'
+#' \dontrun{
 #' # Predict SVM scores
-#' model <- mbr(dataset$train)
-#' pred <- predict(model, dataset$test)
+#' model <- mbr(toyml)
+#' pred <- predict(model, toyml)
 #'
 #' # Predict SVM bipartitions
-#' pred <- predict(model, dataset$test, probability = FALSE)
+#' pred <- predict(model, toyml, probability = FALSE)
 #'
 #' # Passing a specif parameter for SVM predict method
-#' pred <- predict(model, dataset$test, na.action = na.fail)
-predict.MBRmodel <- function(object, newdata, probability = TRUE, ..., CORES = 1) {
-    # Validations
-    if (class(object) != "MBRmodel")
-        stop("First argument must be an MBRmodel object")
+#' pred <- predict(model, toyml, na.action = na.fail)
+#' }
+predict.MBRmodel <- function(object, newdata,
+                             probability = getOption("utiml.use.probs", TRUE),
+                             ..., CORES = getOption("utiml.cores", 1)) {
+  # Validations
+  if (class(object) != "MBRmodel") {
+    stop("First argument must be an MBRmodel object")
+  }
 
-    if (CORES < 1)
-        stop("Cores must be a positive value")
+  if (CORES < 1) {
+    stop("Cores must be a positive value")
+  }
+  newdata <- utiml_newdata(newdata)
 
-    newdata <- utiml_newdata(newdata)
+  # 1 Iteration - Base level -------------------------------------------------
+  base.preds <- predict(object$basemodel, newdata, probability = FALSE, ...,
+                        CORES = CORES)
 
-    # 1 Iteration - Base level
-    base.preds <- predict(object$basemodel, newdata, probability = FALSE, ..., CORES = CORES)
+  # 2 Iteration - Meta level -------------------------------------------------
+  corr <- object$correlation
+  labels <- utiml_renames(object$labels)
+  predictions <- utiml_lapply(labels, function(labelname) {
+    new.col <- colnames(corr)[corr[labelname, ] > object$phi]
+    extra.col <- base.preds[, new.col, drop = FALSE]
+    colnames(extra.col) <- paste("extra", new.col, sep = ".")
+    predict_br_model(object$models[[labelname]], cbind(newdata, extra.col), ...)
+  }, CORES)
 
-    # 2 Iteration - Meta level
-    corr <- object$correlation
-    predictions <- utiml_lapply(object$labels, function(labelname) {
-        extracolumns <- base.preds[, colnames(corr)[corr[labelname, ] > object$phi], drop = FALSE]
-        colnames(extracolumns) <- paste("extra", colnames(extracolumns), sep = ".")
-        predict_br_model(object$models[[labelname]], cbind(newdata, extracolumns), ...)
-    }, CORES)
-    names(predictions) <- object$labels
-
-    as.multilabelPrediction(predictions, probability)
+  as.multilabelPrediction(predictions, probability)
 }
 
+#' Print MBR model
+#' @export
 print.MBRmodel <- function(x, ...) {
-    cat("Classifier Meta-BR (also called 2BR)\n\nCall:\n")
-    print(x$call)
-    cat("\nPhi:", x$phi, "\n")
-    cat("\nCorrelation Table Overview:\n")
-    corr <- x$correlation
-    diag(corr) <- NA
-    tbl <- data.frame(min = apply(corr, 1, min, na.rm = TRUE), mean = apply(corr, 1, mean, na.rm = TRUE), median = apply(corr, 1, median, na.rm = TRUE), max = apply(corr,
-        1, max, na.rm = TRUE), extra = apply(x$correlation, 1, function(row) sum(row > x$phi)))
-    print(tbl)
-}
-
-print.mldMBR <- function(x, ...) {
-    cat("Meta Binary Relevance Transformation Dataset\n\n")
-    cat("Label:\n  ", x$labelname, " (", x$methodname, " method)\n\n", sep = "")
-    cat("Dataset info:\n")
-    cat(" ", ncol(x$data) - 1 - length(x$new.features), "Predictive attributes\n")
-    cat(" ", length(x$new.features), " meta features\n")
-    cat(" ", nrow(x$data), "Examples\n")
-    cat("  ", round((sum(x$data[, ncol(x$data)] == 1)/nrow(x$data)) * 100, 1), "% of positive examples\n", sep = "")
+  cat("Classifier Meta-BR (also called 2BR)\n\nCall:\n")
+  print(x$call)
+  cat("\nPhi:", x$phi, "\n")
+  cat("\nCorrelation Table Overview:\n")
+  corr <- x$correlation
+  diag(corr) <- NA
+  tbl <- data.frame(
+    min = apply(corr, 1, min, na.rm = TRUE),
+    mean = apply(corr, 1, mean, na.rm = TRUE),
+    median = apply(corr, 1, median, na.rm = TRUE),
+    max = apply(corr, 1, max, na.rm = TRUE),
+    extra = apply(x$correlation, 1, function(row) sum(row > x$phi))
+  )
+  print(tbl)
 }
--- R/utilities.R
+++ R/utilities.R
@@ -1,49 +1,47 @@
-#' @title Phi Correlation Coefficient
-#' @family labels correlation
-#' @description Calculate all labels phi correlation coefficient.
-#' This is a specialized version of the Pearson product moment
-#' correlation coefficient for categorical variables with two
-#' values, also called dichotomous variables.
-#' This is also called of Pearson product moment Correlation Coefficient (PCC)
+#' Phi Correlation Coefficient
 #'
-#' @param mdata Object of class \code{\link[mldr]{mldr}}, a multi-label dataset
+#' Calculate all labels phi correlation coefficient. This is a specialized
+#' version of the Pearson product moment correlation coefficient for categorical
+#' variables with two values, also called dichotomous variables. This is also
+#' called of Pearson product moment Correlation Coefficient (PCC)
 #'
-#' @return A matrix with all labels phi correlation coefficient. The rows and
-#' columns have the labels and the values are the phi value. The main diagonal
-#' have the 1 value that represents the correlation of a label with itself.
-#'
+#' @param mdata A mldr multi-label dataset
+#' @return A matrix with all labels correlation coefficient. The rows and
+#'  columns have the labels and each value are the correlation between the
+#'  labels. The main diagonal have the 1 value that represents the correlation
+#'  of a label with itself.
 #' @references
-#'  Tsoumakas, G., Dimou, A., Spyromitros, E., Mezaris, V., Kompatsiaris, I., &
-#'    Vlahavas, I. (2009). Correlation-based pruning of stacked binary relevance models
-#'    for multi-label learning. In Proceedings of the Workshop on Learning from
-#'    Multi-Label Data (MLD’09) (pp. 22–30).
-#'
+#' Tsoumakas, G., Dimou, A., Spyromitros, E., Mezaris, V., Kompatsiaris, I., &
+#'  Vlahavas, I. (2009). Correlation-based pruning of stacked binary relevance
+#'  models for multi-label learning. In Proceedings of the Workshop on Learning
+#'  from Multi-Label Data (MLD’09) (pp. 22–30).
+#' @seealso \code{\link[=mbr]{Meta-BR (MBR or 2BR)}}
 #' @export
 #'
 #' @examples
-#' library(utiml)
-#' result <- labels_correlation_coefficient(emotions)
+#' result <- calculate_labels_correlation(toyml)
 #'
-#' # Get the phi coefficient between the labels 'happy-pleased' and 'quiet-still'
-#' result['happy-pleased', 'quiet-still']
+#' # Get the phi coefficient between the labels 'y1' and 'y2'
+#' result['y1', 'y2']
 #'
 #' # Get all coefficients of a specific label
-#' result[1, ]
-labels_correlation_coefficient <- function(mdata) {
-    labelnames <- rownames(mdata$labels)
-    classes <- mdata$dataset[, mdata$labels$index]
-    q <- length(labelnames)
-    cor <- matrix(nrow = q, ncol = q, dimnames = list(labelnames, labelnames))
-    for (i in 1:q) {
-        for (j in i:q) {
-            confmat <- table(classes[, c(i, j)])
-            A <- as.numeric(confmat["1", "1"])
-            B <- as.numeric(confmat["1", "0"])
-            C <- as.numeric(confmat["0", "1"])
-            D <- as.numeric(confmat["0", "0"])
-            cor[i, j] <- abs((A * D - B * C)/sqrt(as.numeric(A + B) * (C + D) * (A + C) * (B + D)))
-            cor[j, i] <- cor[i, j]
-        }
+#' result[4, -4]
+calculate_labels_correlation <- function(mdata) {
+  label.names <- rownames(mdata$labels)
+  classes <- mdata$dataset[, mdata$labels$index]
+  q <- length(label.names)
+  cor <- matrix(nrow = q, ncol = q, dimnames = list(label.names, label.names))
+  for (i in 1:q) {
+    for (j in i:q) {
+      confmat <- table(classes[, c(i, j)])
+      A <- as.numeric(confmat["1", "1"])
+      B <- as.numeric(confmat["1", "0"])
+      C <- as.numeric(confmat["0", "1"])
+      D <- as.numeric(confmat["0", "0"])
+      cor[i, j] <- abs((A * D - B * C) / sqrt(as.numeric(A + B) * (C + D) *
+                                                (A + C) * (B + D)))
+      cor[j, i] <- cor[i, j]
     }
-    cor
-} 
+  }
+  cor
+}
